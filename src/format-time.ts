83bc861690ab8d5f98248399ccc50a61b47b0ae0
j- <j@skeoh.com>
2020-06-25 13:52:02 +1000

    Fix negative time cases

--- src/format-time.ts
+++ src/format-time.ts
@@ -1,7 +1,8 @@
 export const formatTime = (ms: number) => {
   const negative = ms < 0;
   ms = Math.abs(ms);
-  const m = Math.floor(Math.ceil(ms / 1000) / 60);
-  const s = (negative ? Math.floor(ms / 1000) : Math.ceil(ms / 1000)) % 60;
+  let s = negative ? Math.floor(ms / 1000) : Math.ceil(ms / 1000);
+  const m = Math.floor(s / 60);
+  s %= 60;
   return `${m}:${s < 10 ? '0' : ''}${s}`;
 };
